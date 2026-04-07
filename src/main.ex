954edc77e922c2b327a1111ab52bf26147b2075a
Andreas Wagner <andi@indonesianet.de>
2026-04-07 22:48:00 +0200

    port to phix/euphoria

---
+++ src/main.ex
@@ -0,0 +1,34 @@
+--
+-- Created by maiconpintoabreu on 25/02/2026.
+--
+--adapted to Phix/Euphoria 2026 Andreas Wagner
+include "..\\..\\raylib64.e"
+
+
+include "game_manager.e"
+
+
+
+
+--------------------------------------------------------------------------------------
+-- Program main entry point
+--------------------------------------------------------------------------------------
+function main()
+   {}=Init()
+
+
+    SetTargetFPS(60)    -- Set our game to run at 60 frames-per-second
+
+    -- Main game loop
+    while (UpdateDrawFrame()) do  end while
+
+
+    -- De-Initialization
+    ----------------------------------------------------------------------------------------
+    Destroy()
+    CloseWindow()
+    ----------------------------------------------------------------------------------------
+return 1
+end function
+
+{}=main()
